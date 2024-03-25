266facd1512f83f9d3fe1fce6417b81e490b2e75
Mr-Krabs95 <funt2022@gmail.com>
2024-03-25 23:28:15 +0700

    Started project

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/index.html")
+def read_index(request:Request):
+    return templates.TemplateResponse(request=request,
+    name="index.html")
+
+@app.get("/about.html")
+def read_index(request:Request):
+    return templates.TemplateResponse(request=request,
+    name="about.html")
