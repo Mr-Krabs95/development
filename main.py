9161d79eb1379974a2ecad44876d28ae709cb3fd
Mr-Krabs95 <funt2022@gmail.com>
2024-04-02 23:12:33 +0700

    не пишет в json

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List
+import json
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+# Модель данных для сообщения от формы
+class Message(BaseModel):
+    name: str
+    email: str
+    message: str
+
+# Функция для записи данных в файл JSON
+def write_to_json(data):
+    with open("messages.json", "a") as file:
+        json.dump(data.dict(), file)
+        file.write("\n")
+
+# Маршрут для обработки данных из формы и записи их в файл JSON
+@app.post("/submit-form")
+async def submit_form(data: Message):
+    write_to_json(data)
+    return JSONResponse(content={"message": "Данные успешно сохранены"}, status_code=200)
 
 @app.get("/index.html")
 def read_index(request:Request):
